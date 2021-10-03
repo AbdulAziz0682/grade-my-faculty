dda615c0dda6a90b799369e8dcd02beaa822e296
AbdulAziz0682 <abdulrasheed2132@gmail.com>
2021-10-03 22:47:37 +0500

    Completed Footer and solved MuiStyles injection bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
 
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { StylesProvider, jssPreset, createGenerateClassName } from '@mui/styles';
-import { create } from 'jss';
-import jssExtend from 'jss-plugin-extend';
+import { createTheme, ThemeProvider, StyledEngineProvider } from '@mui/material/styles';
+
+import './index.css';
 
 import Routes from './routes/index';
 
@@ -11,18 +10,12 @@ import themeOptions from './themeOptions';
 
 const theme = createTheme(themeOptions);
 
-const jss = create({
-  ...jssPreset(),
-  plugins: [...jssPreset().plugins, jssExtend()],
-  insertionPoint: document.getElementById('jss-insertion-point'),
-});
-
 export default function App() {
   return (
-    <StylesProvider jss={jss} generateClassName={createGenerateClassName()}>
+    <StyledEngineProvider injectFirst>
       <ThemeProvider theme={theme}>
         <Routes />
       </ThemeProvider>
-    </StylesProvider>
+    </StyledEngineProvider>
   );
 }
--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -3,16 +3,20 @@ import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
-import Facebook from '@mui/icons-material/FacebookRounded';
+import Icon from '@mui/material/Icon';
+
+import facebook from '../../assets/facebook.svg';
+import instagram from '../../assets/instagram.svg';
+import twitter from '../../assets/twitter.svg';
 
 export default function Footer() {
   return (
     <Box id="footer" sx={{ bgcolor: 'primary.main', width: '100%' }}>
       <Container maxWidth="xl" sx={{ py: '50px' }}>
-        <Grid container>
+        <Grid container justifyContent="space-between">
           <Grid
             item
-            md={4}
+            xs={3}
             sx={{
               display: 'flex',
               flexDirection: 'column',
@@ -20,16 +24,16 @@ export default function Footer() {
               gap: 1,
             }}
           >
-            <Typography variant="h6" color="white">Grade My Faculty</Typography>
+            <Typography className="font-semibold" color="white">Grade My Faculty</Typography>
             <div style={{ display: 'flex', flexDirection: 'column', gap: 3 }}>
-              <Typography variant="h6" color="white">FAQ</Typography>
-              <Typography variant="h6" color="white">Contact Us</Typography>
-              <Typography variant="h6" color="white">Blog</Typography>
+              <Typography className="font-semibold" color="white">FAQ</Typography>
+              <Typography className="font-semibold" color="white">Contact Us</Typography>
+              <Typography className="font-semibold" color="white">Blog</Typography>
             </div>
           </Grid>
           <Grid
             item
-            md={6}
+            xs={3}
             sx={{
               display: 'flex',
               flexDirection: 'column',
@@ -37,9 +41,17 @@ export default function Footer() {
               gap: 1,
             }}
           >
-            <Typography variant="h6" color="white" className="bg-yellow-300">Follow Us</Typography>
-            <div style={{ display: 'flex', gap: 3 }}>
-              <Facebook htmlColor="white" />
+            <Typography color="white" className="font-semibold">Follow Us</Typography>
+            <div style={{ display: 'flex', gap: '1.5rem' }}>
+              <Icon>
+                <img src={facebook} alt="facebook" />
+              </Icon>
+              <Icon>
+                <img src={instagram} alt="instagram" />
+              </Icon>
+              <Icon>
+                <img src={twitter} alt="twittter" />
+              </Icon>
             </div>
           </Grid>
         </Grid>
