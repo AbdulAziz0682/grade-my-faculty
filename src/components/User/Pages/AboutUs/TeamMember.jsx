9d3d1de80a38ec0b5c927e540dcbfd5adc048b93
AbdulAziz0682 <abdulrasheed2132@gmail.com>
2022-02-14 19:30:49 +0500

    Added img src and changed icon in TeamMembers

---
+++ src/components/User/Pages/AboutUs/TeamMember.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import Typography from '@mui/material/Typography';
+import LinkedIn from '@mui/icons-material/LinkedIn';
+import Facebook from '@mui/icons-material/Facebook';
+import Instagram from '@mui/icons-material/Instagram';
+import Icon from '@mui/material/Icon';
+
+export default function TeamMember({ member }) {
+  return (
+    <>
+      <img src={member.imgSrc} alt={member.name} className="w-20" />
+      <Typography align="center" className="font-semibold text-medium">{member.name}</Typography>
+      <Typography align="center" className="text-sm">{member.role}</Typography>
+      <div
+        style={{
+          display: 'flex', gap: '1rem', justifyContent: 'space-between',
+        }}
+      >
+        <a href={member.facebookLink}>
+          <Icon className="flex">
+            <Facebook color="primary" className="w-6 h-6" />
+          </Icon>
+        </a>
+        <a href={member.instagramLink}>
+          <Icon className="flex">
+            <Instagram color="primary" className="w-6 h-6" />
+          </Icon>
+        </a>
+        <a href={member.linkedinLink}>
+          <Icon className="flex">
+            <LinkedIn color="primary" className="w-6 h-6" />
+          </Icon>
+        </a>
+      </div>
+    </>
+  );
+}
+
+TeamMember.propTypes = {
+  member: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    role: PropTypes.string.isRequired,
+    imgSrc: PropTypes.string.isRequired,
+    facebookLink: PropTypes.string.isRequired,
+    instagramLink: PropTypes.string.isRequired,
+    linkedinLink: PropTypes.string.isRequired,
+  }).isRequired,
+};
--- src/components/User/Pages/AboutUs/index.jsx
+++ src/components/User/Pages/AboutUs/index.jsx
@@ -1,31 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import Card from '@mui/material/Card';
 import CircularProgress from '@mui/material/CircularProgress';
+import Icon from '@mui/material/Icon';
+
+import AliceCarousel from 'react-alice-carousel';
+import 'react-alice-carousel/lib/alice-carousel.css';
 
 import { useQuery } from '@apollo/client';
 
-import FacultyProfile from './FacultyProfile';
+import TeamMember from './TeamMember';
 import { ABOUT_US } from '../../../../graphqlQueries';
+
+import arrowLeft from '../../../../assets/arrow-left.svg';
+import arrowRight from '../../../../assets/arrow-right.svg';
 
 export default function AboutUs() {
   const list = [
     {
       name: 'Utsho',
+      imgSrc: '',
+      role: 'Founder',
       facebookLink: 'https://www.facebook.com/utsho1214',
       instagramLink: 'https://www.instagram.com/tef.utsho/',
       linkedinLink: 'https://www.linkedin.com/in/tasnim-ferdaous/',
     },
     {
       name: 'Sakib',
+      imgSrc: '',
+      role: 'Co-founder',
       facebookLink: 'https://www.facebook.com/sakibbzz/',
       instagramLink: 'https://www.instagram.com/sakibbzz/',
       linkedinLink: 'https://www.linkedin.com/in/sadmansakibrabbani/',
     },
+    {
+      name: 'Utsho',
+      imgSrc: '',
+      role: 'Founder',
+      facebookLink: 'https://www.facebook.com/utsho1214',
+      instagramLink: 'https://www.instagram.com/tef.utsho/',
+      linkedinLink: 'https://www.linkedin.com/in/tasnim-ferdaous/',
+    },
   ];
+  const [activeIndex, setActiveIndex] = useState(0);
+  function handlePrevious() {
+    let prevIndex = activeIndex - 1;
+    if (prevIndex < 0) {
+      prevIndex = list.length - 1;
+    }
+    setActiveIndex(() => prevIndex);
+  }
+  function handleNext() {
+    let nextIndex = activeIndex + 1;
+    if (nextIndex >= list.length) {
+      nextIndex = 0;
+    }
+    setActiveIndex(() => nextIndex);
+  }
   const { loading, data } = useQuery(ABOUT_US);
   if (loading) return <div className="absolute inset-x-0 flex items-center justify-center mt-16"><CircularProgress /></div>;
   return (
@@ -43,17 +77,40 @@ export default function AboutUs() {
               </Typography>
             </Card>
           </Grid>
-          <Grid item className="flex flex-col items-center justify-between gap-2 md:flex-row">
-            <Card className="w-full p-3 mx-auto flexm md:w-5/6 lg:w-7/12">
-              <Grid container rowSpacing={2}>
-                {
+          <Grid item className="flex flex-wrap items-center justify-between w-full gap-2">
+            <Card className="flex w-full h-auto cursor-pointer md:w-auto" onClick={() => handlePrevious()}>
+              <Icon className="flex w-12 h-12 p-2 mx-auto">
+                <img src={arrowLeft} alt="arrow left" className="w-full" />
+              </Icon>
+            </Card>
+            <Card className="relative flex w-full p-3 mx-auto md:w-5/6 lg:w-7/12">
+              <AliceCarousel
+                autoPlay
+                mouseTracking
+                activeIndex={activeIndex}
+                disableButtonsControls
+                disableDotsControls
+                infinite
+                onSlideChange={(e) => setActiveIndex(e.item)}
+                animationDuration={3000}
+                responsive={{
+                  0: { items: 1 },
+                  600: { items: 2 },
+                  900: { items: 3 },
+                }}
+                items={
                   list.map((item) => (
-                    <Grid item xs={6} className="flex flex-col items-center">
-                      <FacultyProfile faculty={item} />
-                    </Grid>
+                    <div className="flex flex-col items-center mx-auto">
+                      <TeamMember member={item} />
+                    </div>
                   ))
                 }
-              </Grid>
+              />
+            </Card>
+            <Card className="flex w-full h-auto cursor-pointer md:w-auto" onClick={() => handleNext()}>
+              <Icon className="flex w-12 h-12 p-2 mx-auto">
+                <img src={arrowRight} alt="arrow left" className="w-full" />
+              </Icon>
             </Card>
           </Grid>
           <Grid item className="flex items-center justify-center">
